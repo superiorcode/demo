5d602e58704d6fadf57e6caea893574e84c4ac22
joker <joker@qq.com>
2021-09-07 20:38:15 +0800

    hotfix commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello,gi3333");
+        System.out.println("hello,git1");
+        System.out.println("hello,git2");
+        System.out.println("hello,git3");
     }
 }
